92803260eb0e864fdaeaeab807978b578a7a40b5
Wenega Wama <narcissewatherloy@gmail.com>
2021-08-04 19:27:18 -0300

    second commit

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+app.use(express.json())
+const mongoose = require('mongoose')
+const router = require('express').Router()
+const authRoute = require('../api/routes/auth')
+const userRoute = require('../api/routes/users')
+const postRoute = require('../api/routes/posts')
+
+mongoose.connect(process.env.MONGO_URL_CONNECT,
+    {
+        useNewUrlParser: true ,
+        useUnifiedTopology: true,
+        useCreateIndex:true 
+    }).then(() => console.log('Conectou com sucesso com o MongoDB')).catch((err) => console.log(err))
+    //console.log('nome do banco de dados', process.env.MONGO_DB)
+    //console.log('nome do usuario', process.env.MONGO_USER)
+    //console.log('nome do usuario', process.env.MONGO_PASSWORD)
+    
+
+app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
+app.use('/api/posts', postRoute)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log("Iniciandoo o servidor express.....")
+})
+
+
+
+
+
+
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const User = require('../models/User')
+const Post = require('../models/Post')
 const bcrypt = require('bcrypt')
 
 //Update
@@ -24,24 +25,35 @@ router.put("/:id", async (req, res) => {
 })
 
 //Delete
-router.delete("/:id", async (req, res) => {         
+router.delete("/:id", async (req, res) => {   
+    if(req.body.userId === req.params.id) {
         try {
-            const post = await Post.findById(req.params.id)
-            if(post.username === req.body.username) {
-                try {
-                    await post.delete
-                    res.status(200).json(" O Usuário é deletado com sucesso...")
-                } catch(err){
-                    res.status(500).json(err)           
-                }
-            } else {
-                res.status(401).json("Deleção recusada, faça com a sua conta !")
-            } 
+            const user = await User.findById(req.params.id)
+            try{
+                await Post.deleteMany({username: user.username})
+                await User.findByIdAndDelete(req.params.id)
+                res.status(200).json(" O Usuário é deletado com sucesso...")
             } catch(err){
-                res.status(404).json("Usuário não encontrado")
-            }                
+                res.status(500).json(err)
+            }
+        } catch(err){
+            res.status(404).json("Usuario não encontrado")           
+            }
+    } else {
+        res.status(401).json("Deleção recusada, faça com a sua conta !")
+    }            
 })
 
-//Create
-//Read
+//Get usuário
+
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await  User.findByIdAndUpdate(req.params.id)
+        const {senha, ...outros} = user._doc
+        res.status(200).json(outros)
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
 module.exports = router
