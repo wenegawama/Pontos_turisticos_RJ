ebbd7e45f2235709e1600e8181e544da5e3fa4a8
Wenega Wama <narcissewatherloy@gmail.com>
2021-08-14 00:26:01 -0300

    react router funcionando

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,32 @@ import Postar from './pages/postar/Postar'
 import Modificacao from './pages/modificacao/Modificacao'
 import Login from './pages/login/Login'
 import Registrar from './pages/registrar/Registrar'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
+  const user = false
   return (
-    <>
+    <Router>
       <NavBar />
-      <Registrar/>
-    </>
+      <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/registrar">{user ? <Home /> :<Registrar />}</Route>
+          <Route path="/login">{user ? <Home /> :<Login />}</Route>
+          <Route path="/postar">{user ? <Postar /> :<Registrar />}</Route>
+          <Route path="/modificacao">{user ? <Modificacao /> :<Registrar />}</Route>
+          <Route path="/post/:postId">
+            <Unica />
+          </Route>
+      </Switch>
+    </Router>
   );
 }
 
