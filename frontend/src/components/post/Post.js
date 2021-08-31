ed0b0a76affb48b328da68184fb3e3944677fedd
Wenega Wama <narcissewatherloy@gmail.com>
2021-08-31 17:52:55 -0300

    Dinâmico categoria

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -4,13 +4,18 @@ function Post({post}) {
     return (
         
         <div className="post">
-            
-                    <img className="postImg" src='https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt="" />
+            {post.foto && (
+                <img className="postImg" src={post.foto} alt="" />
+
+            )}
             
             <div className="postInfo">
                 <div className="postCats">
-                    <span className="postCats">Praias</span>
-                    <span className="postCats">Cinemas</span>
+                    {post.categories.map((c) => (
+                        <span className="postCats">{c.name}</span>
+
+                    ))}
+                    
                 </div>
                 <span className="postTitulo">{post.titulo}</span>
                 
